4e80ded624900fac9f9565987fe20cb2114f879c
Nick Carlucci <nicholas.r.carlucci@gmail.com>
2020-08-07 08:54:31 -0400

    hooked up table component to pull data from database

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,49 +1,42 @@
 import React from "react";
 import { Table } from "semantic-ui-react";
 
-const teams = [
-  {
-    name: "ButtMunchers",
-    wins: 4,
-    losses: 3,
-  },
-  {
-    name: "SquiddlyDiddlies",
-    wins: 2,
-    losses: 5,
-  },
-  {
-    name: "Foosballers",
-    wins: 5,
-    losses: 2,
-  },
-  {
-    name: "Brad and Nick's Ambiguously Gay Duo",
-    wins: 7,
-    losses: 0,
-  },
-];
 
-const StandingsTable = () => (
-  <Table celled>
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell>Team Name</Table.HeaderCell>
-        <Table.HeaderCell>Wins</Table.HeaderCell>
-        <Table.HeaderCell>Losses</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
+class StandingsTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {teams: [{name: "Something", wins: 1, losses: 6}]};
+  }
 
-    <Table.Body>
-      {teams.map((team, index) => (
-        <Table.Row>
-          <Table.Cell>{team.name}</Table.Cell>
-          <Table.Cell>{team.wins}</Table.Cell>
-          <Table.Cell>{team.losses}</Table.Cell>
-        </Table.Row>
-      ))}
-    </Table.Body>
-  </Table>
-);
+  async componentDidMount() {
+    const response = await fetch("http://localhost:3001");
+    const json = await response.json();
+    this.setState({teams: json});
+  }
+
+  render(){
+    return (
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Team Name</Table.HeaderCell>
+            <Table.HeaderCell>Wins</Table.HeaderCell>
+            <Table.HeaderCell>Losses</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {this.state.teams.map((team, index) => (
+            <Table.Row>
+              <Table.Cell>{team.name}</Table.Cell>
+              <Table.Cell>{team.wins}</Table.Cell>
+              <Table.Cell>{team.losses}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    );
+  }
+}
 
 export default StandingsTable;
